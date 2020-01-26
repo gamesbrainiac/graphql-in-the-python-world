9e5431469e4e1b51cf3a9d5df2e4049670eb823b
Navid Dezashibi <dezashibi@gmail.com>
2020-01-26 10:38:34 +0330

    Updating project for new version support

--- schema.py
+++ schema.py
@@ -12,19 +12,23 @@ class Query(graphene.ObjectType):
                           id=graphene.Int(),
                           username=graphene.String())
 
-    def resolve_user(self, args, context, info):
-        query = UserObject.get_query(context)
+    def resolve_user(self, info, **args):
+        query = UserObject.get_query(info)
         query = query.filter_by(**args)
         return query.first()
 
 
 class Mutations(graphene.ObjectType):
 
-    create_user = CreateUser.Field(description='Creates a new user. Given a username and email.')
+    create_user = CreateUser.Field(
+        description='Creates a new user. Given a username and email.')
     delete_user = DeleteUser.Field(description='Deletes a user. Given the id.')
-    create_todo = CreateTodo.Field(description='Creates a todo given description and user id')
-    delete_todo = DeleteTodo.Field(description='Deletes a todo given the id and user.')
-    add_todo_item = AddTodoItem.Field(description='Adds an item to an existing todo list')
+    create_todo = CreateTodo.Field(
+        description='Creates a todo given description and user id')
+    delete_todo = DeleteTodo.Field(
+        description='Deletes a todo given the id and user.')
+    add_todo_item = AddTodoItem.Field(
+        description='Adds an item to an existing todo list')
     delete_todo_item = DeleteTodoItem.Field()
     complete_todo_item = CompleteTodoItem.Field()
 
